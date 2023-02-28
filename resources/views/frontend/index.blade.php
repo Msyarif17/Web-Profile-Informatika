98fa4a6aa9c34645fa3285ed54d831bad7ca1b9e
unknown <m.syarifsetiadi@gmail.com>
2023-02-28 11:53:39 +0700

    fixing error component

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -165,7 +165,7 @@
                             @if ($i == 0)
                                 <div class="carousel-item active">
                                     <img src="{{ asset('storage' . $p->banner) }}" class="d-block w-100" alt="...">
-                                    <div class="carousel-caption d-none d-md-block"  style="background: rgb(0,0,0);
+                                    <div class="carousel-caption d-none d-md-block w-100"  style="background: rgb(0,0,0);
                                     background: linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(255,255,255,0) 100%);">
                                         <h5 class="text-capitalize  text-shadow fw-bold">{{ $p->title }}</h5>
                                         <p>{{ Str::limit(strip_tags($p->content), 100, '...') }}</p>
@@ -174,7 +174,7 @@
                             @else
                                 <div class="carousel-item">
                                     <img src="{{ asset('storage' . $p->banner) }}" class="d-block w-100" alt="...">
-                                    <div class="carousel-caption d-none d-md-block" style="background: rgb(0,0,0);
+                                    <div class="carousel-caption d-none d-md-block w-100" style="background: rgb(0,0,0);
                                     background: linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(255,255,255,0) 100%);">
                                         <h5 class="text-capitalize text-shadow fw-bold">{{ $p->title }}</h5>
                                         <p>{{ Str::limit(strip_tags($p->content), 100, '...') }}</p>
