73ee29e8539b33ae4c973f05cee2d579a3c670cb
unknown <m.syarifsetiadi@gmail.com>
2023-02-28 11:19:26 +0700

    fixing error component

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -31,11 +31,11 @@ class IndexController extends Controller
         $socials = SocialMedia::all();
         $pengumuman = CategoryPost::where('slug','LIKE', '%pengumuman%')->latest()->first();
         $prestasi = CategoryPost::where('slug','LIKE', '%prestasi%')->latest()->first();
-        if ($pengumuman)
+        if (isset($pengumuman))
             $pengumuman = $data->where('category_post_id', $pengumuman->id)->latest()->take(3)->get();
         else
             $pengumuman = [];
-        if ($prestasi)
+        if (isset($prestasi))
             $prestasi = $data->where('category_post_id', $prestasi->id)->latest()->take(3)->get();
         else
             $pengumuman = [];
