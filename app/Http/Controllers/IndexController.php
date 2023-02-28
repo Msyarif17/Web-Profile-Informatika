c4b337084d768aa995cd641514ca836914bbc7d1
unknown <m.syarifsetiadi@gmail.com>
2023-02-28 11:10:49 +0700

    fixing error component

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -29,8 +29,8 @@ class IndexController extends Controller
         $peminat = PeminatJurusan::pluck('peminat','tahun_akademik')->all();
         $footer = Footer::get()->all();
         $socials = SocialMedia::all();
-        $pengumuman = CategoryPost::where('slug', 'pengumuman')->orWhere('slug','Pengumuman')->latest()->first();
-        $prestasi = CategoryPost::where('slug', 'prestasi')->orWhere('slug','Prestasi')->latest()->first();
+        $pengumuman = CategoryPost::where('slug','LIKE', '%pengumuman%')->latest()->first();
+        $prestasi = CategoryPost::where('slug','LIKE', '%prestasi%')->latest()->first();
         if ($pengumuman)
             $pengumuman = $data->where('category_post_id', $pengumuman->id)->latest()->take(3)->get();
         else
